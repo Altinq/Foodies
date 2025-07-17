f1b2531bceaa53a49ccc3d9d95114f338a26e8da
Altinq <altin.qunaku@gmail.com>
2025-07-17 16:37:41 +0200

    Add meals loading and error handling components; implement async data fetching

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page Not Found</h1>
+      <p>Sorry, we couldn't find the page you were looking for.</p>
+    </main>
+  );
+}
