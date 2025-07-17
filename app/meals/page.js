67907f8685dbdd2f912b0a2dce72866fc93038d1
Altinq <altin.qunaku@gmail.com>
2025-07-17 15:43:44 +0200

    added mealitem component, meals-grid, added styling, added the logic to show meals in meals path, created a local database using sqlite3

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,13 +1,24 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
 import Link from "next/link";
-
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Meals Page</h1>
-      <p>Welcome to the meals page!</p>
-      <p>
-        <Link href="/meals/share">Go to Share Page</Link>
-      </p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Chose your favorite recipe and cook it yourself. It is easy and fun
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
   );
 }
