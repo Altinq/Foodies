126b1c8ef7a321ffaa25fe8d3c8f47578892a8a0
Altinq <altin.qunaku@gmail.com>
2025-07-18 10:36:51 +0200

    Implement ShareMealPage with form submission and image upload functionality; enhance ImagePicker for image preview

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,17 +1,41 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 export default function ImagePicker({ label, name }) {
+  const [selectedImage, setSelectedImage] = useState();
   const imageInput = useRef();
 
   function handleImagePick() {
     imageInput.current.click();
   }
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setSelectedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      setSelectedImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!selectedImage && <p>No image chosen.</p>}
+          {selectedImage && (
+            <Image src={selectedImage} alt="Selected Image" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -19,6 +43,8 @@ export default function ImagePicker({ label, name }) {
           name={name}
           accept="image/png, image/jpeg"
           ref={imageInput}
+          onChange={handleImageChange}
+          required
         />
         <button
           type="button"
